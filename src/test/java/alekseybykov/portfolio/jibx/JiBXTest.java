6347bd08fdd3e6fc90991c488bfb5b4ac61c1656
AlekseyBykov <aleksey.n.bykov@gmail.com>
2020-05-04 10:10:43 +0400

    Refactoring.

--- src/test/java/alekseybykov/portfolio/jibx/JiBXTest.java
+++ src/test/java/alekseybykov/portfolio/jibx/JiBXTest.java
@@ -3,18 +3,14 @@ package alekseybykov.portfolio.jibx;
 import org.jibx.runtime.JiBXException;
 import org.junit.Test;
 
-import java.io.FileNotFoundException;
-import java.io.InputStream;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 public class JiBXTest extends JiBXTestBase {
 
 	@Test
-	public void test() throws FileNotFoundException, JiBXException {
-		InputStream inputStream = getClass().getClassLoader().getResourceAsStream("book.xml");
-		Book book = (Book) unmarshallingContext.unmarshalDocument(inputStream, null);
+	public void test() throws JiBXException {
+		Book book = (Book) unmarshallingContext.unmarshalDocument(loadXml("book.xml"), null);
 		assertNotNull(book);
 
 		assertEquals("Name", book.getName());
--- src/test/java/alekseybykov/portfolio/jibx/JiBXTestBase.java
+++ src/test/java/alekseybykov/portfolio/jibx/JiBXTestBase.java
@@ -6,13 +6,21 @@ import org.jibx.runtime.IUnmarshallingContext;
 import org.jibx.runtime.JiBXException;
 import org.junit.BeforeClass;
 
+import java.io.InputStream;
+
 public class JiBXTestBase {
 
+	private static ClassLoader classLoader;
 	protected static IUnmarshallingContext unmarshallingContext;
 
 	@BeforeClass
 	public static void setUp() throws JiBXException {
 		IBindingFactory bindingFactory = BindingDirectory.getFactory(Book.class);
 		unmarshallingContext = bindingFactory.createUnmarshallingContext();
+		classLoader = JiBXTestBase.class.getClassLoader();
+	}
+
+	protected InputStream loadXml(String xml) {
+		return classLoader.getResourceAsStream(xml);
 	}
 }
